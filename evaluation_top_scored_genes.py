ca04e5ea9c77d8ec0559f5843c81396554575d74
Hongdong Li <48688298+genemine@users.noreply.github.com>
2020-04-01 15:37:38 +0800

    Update evaluation_top_scored_genes.py

--- evaluation_top_scored_genes.py
+++ evaluation_top_scored_genes.py
@@ -12,10 +12,10 @@ import numpy as np
 import random
 
 
-def p_value(random_list,obeserved):
+def p_value(random_list,observed):
     count=0
     for item in random_list:
-        if item > obeserved:
+        if item > observed:
             count = count+1
     if count == 0:
         p_val = 0.0001
@@ -23,12 +23,12 @@ def p_value(random_list,obeserved):
         p_val = count*1.0/times
     return p_val
 
-def write_file(p_val,obeserved,randomx,f):
+def write_file(p_val,observed,randomx,f):
     line = 'p-value:'+str(p_val)+'\n'
     print(line)
     f.write(line)
-    obeserved=np.round(obeserved,4)
-    line = 'obeserved:'+str(obeserved)+'\n'
+    observed=np.round(observed,4)
+    line = 'observed:'+str(observed)+'\n'
     print(line)
     f.write(line)
     randomx=np.round(randomx,4)
@@ -43,10 +43,10 @@ def write_file(p_val,obeserved,randomx,f):
 def seq_validation(genelist,num,times):
     print('Step1: evaluation of sequence similarity with known genes')
     score=eval(open('data/sequence_similarity.txt').read())
-    obeserved = []
+    observed = []
     for item in genelist[0:num]:
         if item in score:
-            obeserved.append(score[item])
+            observed.append(score[item])
     random_score= [0]*times
     for i in range(times):
         random_list = genelist[random.sample(range(len(genelist)),num)]
@@ -54,12 +54,12 @@ def seq_validation(genelist,num,times):
             if item in score:
                 random_score[i] = random_score[i]+score[item]
         random_score[i] = random_score[i]/num 
-    obeserved = np.round(np.mean(obeserved),4)
+    observed = np.round(np.mean(observed),4)
     randomx= np.round(np.mean(random_score),4)
-    p_val=p_value(random_score,obeserved)
+    p_val=p_value(random_score,observed)
     f=open('validation_result/seq_evaluation.txt','w')
-    write_file(p_val,obeserved,randomx,f)
-    return p_val,obeserved,random_score
+    write_file(p_val,observed,randomx,f)
+    return p_val,observed,random_score
 
 
 
@@ -75,17 +75,17 @@ def coexpr_validation(num,genelist,times):
     data=data.loc[list(genelist),:]
     data=data.abs().mean(axis=1)
     gene=genelist[:num]
-    obeserved=data[gene].mean()
+    observed=data[gene].mean()
     random_score=[]
     for i in range(times):
         gene=genelist[random.sample(range(len(genelist)),num)]
         random_score.append(data[gene].mean())
     randomx=np.median(random_score)
-    pval=p_value(random_score,obeserved)
+    pval=p_value(random_score,observed)
     print('score of coexpression with AD-associated genes')
     f=open('validation_result/coexpr_score_evaluation.txt','w')
-    write_file(pval,obeserved,randomx,f)
-    return pval,obeserved,random_score
+    write_file(pval,observed,randomx,f)
+    return pval,observed,random_score
 
 
 
@@ -99,7 +99,7 @@ def ppi_validation(ad_gene,num,genelist,times):
         stats_ppi_gene=stats.loc[gene,:]
         return stats_ppi_gene.mean()['score'],stats_ppi_gene.mean()['num']
     gene=genelist[:num]
-    obeserved_score,obeserved_num=stats_ppi(gene,stats)
+    observed_score,observed_num=stats_ppi(gene,stats)
     random_score=[]
     random_num=[]
     for i in range(times):
@@ -107,16 +107,16 @@ def ppi_validation(ad_gene,num,genelist,times):
         x1,x2=stats_ppi(gene,stats)
         random_score.append(x1)
         random_num.append(x2)
-    pval_score=p_value(random_score,obeserved_score)
-    pval_num=p_value(random_num,obeserved_num)
+    pval_score=p_value(random_score,observed_score)
+    pval_num=p_value(random_num,observed_num)
     randomx=np.median(random_score)
     f=open('validation_result/ppi_score_evaluation.txt','w')
     print('score of ppi interaction with AD-associated genes')
-    write_file(pval_score,obeserved_score,randomx,f)
+    write_file(pval_score,observed_score,randomx,f)
     f=open('validation_result/ppi_num_evaluation.txt','w')
     print('number of ppi interaction with AD-associated genes')
     randomx=np.median(random_num)
-    write_file(pval_num,obeserved_num,randomx,f)
+    write_file(pval_num,observed_num,randomx,f)
 
 
 
@@ -141,12 +141,12 @@ def mirna_validation(ad_gene,num,genelist,times):
         random_numi=len(mirna_random.intersection(mirna_ad))
         random_num.append(random_numi)
     mirna_top=mirnaOfgenes(genelist[0:num],gene_mirna)
-    obeserved=len(mirna_top.intersection(mirna_ad))
-    p_val=p_value(random_num,obeserved)
+    observed=len(mirna_top.intersection(mirna_ad))
+    p_val=p_value(random_num,observed)
     randomx=np.mean(random_num)
     f=open('validation_result/mirna_evaluation.txt','w')
-    write_file(p_val,obeserved,randomx,f)
-    return p_val,obeserved,random_num
+    write_file(p_val,observed,randomx,f)
+    return p_val,observed,random_num
 
 ### main ###
     
@@ -164,12 +164,12 @@ for item in genes_all:
         genelist.append(item)
 genelist = np.array(genelist)
 
-seq_pval,seq_obeserved,seq_random_score=seq_validation(genelist,num,times)
+seq_pval,seq_observed,seq_random_score=seq_validation(genelist,num,times)
 
-coexp_pval,coexp_obeserved,coexp_random_score=coexpr_validation(num,genelist,times)
+coexp_pval,coexp_observed,coexp_random_score=coexpr_validation(num,genelist,times)
 
 ppi_validation(ad_gene,num,genelist,times)
 
-miRNA_pval,miRNA_obeserved,miRNA_random_num=mirna_validation(ad_gene,num,genelist,times)      
+miRNA_pval,miRNA_observed,miRNA_random_num=mirna_validation(ad_gene,num,genelist,times)      
 
 
